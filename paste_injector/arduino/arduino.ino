961fef4d41485cc9522de8c3eb982f26773b31b1
Zapta <zapta@zapta.com>
2015-08-26 13:49:15 -0700

    Cleaned up the button logic. Added anti oozing.

--- paste_injector/arduino/arduino.ino
+++ paste_injector/arduino/arduino.ino
@@ -1,57 +1,111 @@
+// Controller the solder paste injector.
+
 #include <AccelStepper.h>
 
 const int kHalfStep = 8;
 
 // Arduino output pins to the 28BYJ-48 motor driver.
-const int  kMotorPin1 = 6;     // IN1 on the ULN2003 driver 1
-const int  kMotorPin2 = 7;     // IN2 on the ULN2003 driver 1
-const int  kMotorPin3 = 8;     // IN3 on the ULN2003 driver 1
-const int  kMotorPin4 = 9;     // IN4 on the ULN2003 driver 1
+const int  kMotorPin1 = 6;     
+const int  kMotorPin2 = 7;    
+const int  kMotorPin3 = 8;     
+const int  kMotorPin4 = 9;     
 
-const int kButton1 = 10;
-const int kButton2 = 11;
+const int kForwardButtonPin = 10;
+const int kBackwardButtonPin = 11;
 
-const int kMinDistance = 250;
+const int kForwardSteps = 400;
+const int kBacklashSteps = 200;
+const int kBackwardSteps = 300;
 
-// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 
+const int kForwardSpeed = 500;
+const int kBacklashSpeed = 1000;
+const int kBackwardSpeed = 1000;
 
-AccelStepper stepper(kHalfStep, kMotorPin1, kMotorPin3, kMotorPin2, kMotorPin4);
+//const int kMinDistance = 250;
 
-const int kSpeed = 500; //speed of the stepper (steps per second)
+// NOTE: the pins are not listed by their numeric order.
+AccelStepper stepper(kHalfStep, 
+    kMotorPin1, 
+    kMotorPin3, 
+    kMotorPin2, 
+    kMotorPin4);
 
-// True -> apply pressue. 
-boolean direction = true; //keep track if we are turning or going straight next
+enum State {
+  IDLE,
+  FORWARD,
+  BACKLASH,
+  BACKWARD,
+};
+
+State state = IDLE;
+
+inline bool isForwardButtonPressed() {
+  // Active low.
+  !digitalRead(kForwardButtonPin);  
+}
+
+inline bool isBackwardButtonPressed() {
+  // Active low.
+  !digitalRead(kBackwardButtonPin);  
+}
+
+// Steps: positive -> extrude.
+void setMotion(int steps, int speed) {
+  stepper.move(steps);
+  stepper.setSpeed(speed);
+}
 
 void setup() {
-
    // initialize the pushbutton pin as an input:
-  pinMode(kButton1, INPUT_PULLUP);  
-  pinMode(kButton2, INPUT_PULLUP);  
-  
-  delay(1000); //sime time to put the robot down after swithing it on
+  pinMode(kForwardButtonPin, INPUT_PULLUP);  
+  pinMode(kBackwardButtonPin, INPUT_PULLUP);  
 
-  stepper.setMaxSpeed(4000.0);
-  
-  stepper.move(1);  // I found this necessary
-  stepper.setSpeed(kSpeed);
+  //delay(1000); //sime time to put the robot down after swithing it on
+
+  stepper.setMaxSpeed(1000.0);
+
+  setMotion(1, 500);
 }
-void loop() {
 
-  if (!stepper.distanceToGo()) {
-    int target = 0;
-    if (!digitalRead(kButton1)) {
-      target = kMinDistance;
-    } else if (!digitalRead(kButton2)) {
-      target = -kMinDistance;
-    }
-    //onst int target = direction ? 20000 : -30000;
-    if (target) {
-      stepper.move(target);
-      stepper.setSpeed(kSpeed);
-    }
-    direction = !direction;
+void loop() {
+  stepper.runSpeedToPosition();
+
+  if (stepper.distanceToGo()) {
+    return;
   }
 
-  stepper.runSpeedToPosition();
-//  stepper2.runSpeedToPosition();
+  // Here when last motion completed.
+  switch (state) {
+    case IDLE:
+    case BACKLASH:
+    case BACKWARD:
+      // Forward button.
+      if (isForwardButtonPressed()) { 
+        setMotion(kForwardSteps, kForwardSpeed); 
+        state = FORWARD;
+      // Backward button.
+      } else if (isBackwardButtonPressed()) { 
+        setMotion(-kBackwardSteps, kBackwardSpeed);
+        state = BACKWARD;
+      // Default, be in IDLE state.
+      } else {
+        state = IDLE;
+      }
+      break;
+
+    case FORWARD:
+      // Forward button: continue forward.
+      if (isForwardButtonPressed()) { 
+        setMotion(kForwardSteps, kForwardSpeed); 
+        state = FORWARD;
+        return;
+      }     
+      // Forward done: do backlash to reduce oozing.
+      setMotion(-kBacklashSteps, kBacklashSpeed);
+      state = BACKLASH;
+      break;
+
+    default:
+      state = IDLE;
+  }
 }
