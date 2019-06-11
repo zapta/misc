938bae9bc45cc3e8d35803597144b2b973ff866c
Zapta <zapta@zapta.com>
2019-06-10 19:30:33 -0700

    Added tracking of traffic state. A short period of no updates is considered as BUSY.

--- duet3d_monitor/arduino/arduino.ino
+++ duet3d_monitor/arduino/arduino.ino
@@ -7,7 +7,7 @@
 
 // Onboard Arduino led. Used for diagnostics.
 #define LED (13)
-static PassiveTimer led_timer;
+//static PassiveTimer led_timer;
 
 // Max luminicity in the range [0-255].
 #define L 255
@@ -23,7 +23,17 @@ static const rgb_led::Color AT_REST_COLOR    = rgb_led::make_color(0,   L,   0);
 // We use the arduino on board LED to indicate reception of status
 // report messages. Each message triggers a short blip.
 static PassiveTimer traffic_timer;
-static bool traffic_timeout = false;
+
+enum TrafficState {
+  // Had a recent status report message
+  OK,
+  // Didn't have a status report for a medium period. Assuming paneldue busy.
+  BUSY,
+  // Didn't have a status report for a long time.
+  ERROR
+};
+
+static TrafficState traffic_state = OK;
 
 void setup() {
   rgb_led::setup();
@@ -38,7 +48,7 @@ void setup() {
 
   // Start with a single led ping
   digitalWrite(LED, HIGH);
-  led_timer.restart();
+  //traffic_timer.restart();
 
   // A quick RGB test pattern
   delay(300);
@@ -54,44 +64,50 @@ void setup() {
 void loop() {
   rgb_led::loop();
 
-  // Turn off diagnostic led ping after timeout.
-  if (digitalRead(LED) && led_timer.timeMillis() > 10) {
-    digitalWrite(LED, LOW);
-  }
-
   // Process pending serial chars.
   while (HWSERIAL.available() > 0) {
     const char c = (char)HWSERIAL.read();
     monitor::ProcessNextChar(c);
   }
 
-  // Report events, if any. For diagnostics.
+  // Report events, if any. For diagnostics. We ignore traffic event
+  // to reduce spam.
   const int events = monitor::ConsumePendingEvents();
   if (events & ~monitor::HAD_TRAFFIC) {
     Serial.print("Events: ");
     Serial.println(events);
   }
 
-  // If a status message wass detected, ping onboard LED. For diagnostics.
-  if (events & (monitor::REPORTED_ACTIVE | monitor::REPORTED_COOLING | monitor::REPORTED_AT_REST)) {
-    digitalWrite(LED, HIGH);
-    led_timer.restart();
+  // Turn off diagnostic led ping after a short timeout.
+  if (digitalRead(LED) && traffic_state == OK && traffic_timer.timeMillis() > 10) {
+    digitalWrite(LED, LOW);
   }
-
-  // Track serial PanelDue communiction traffic timeout
+  
+  // Track serial PanelDue communiction traffic state. We use the reports
+  // events as a proxy for the traffic.
   // We use a long timeout since the duet does not report while executing
   // commands such as homming or messing.
-  if (events & monitor::HAD_TRAFFIC) {
-    traffic_timeout = false;
+  const bool got_report =   (events & (monitor::REPORTED_ACTIVE | monitor::REPORTED_COOLING | monitor::REPORTED_AT_REST));
+  if (got_report) {
+    digitalWrite(LED, HIGH);
+    traffic_state = OK;
     traffic_timer.restart();
-  } else if (!traffic_timeout && traffic_timer.timeMillis() > 10 * 60 * 1000) {
-    traffic_timeout = true;
+  } else if (traffic_state == OK && traffic_timer.timeMillis() > 2500) {
+    // Missed a few report. Assume PanelDue started a blocking operation.
+    traffic_state = BUSY;
+    traffic_timer.restart();
+  } else if (traffic_state == BUSY && traffic_timer.timeMillis() > 600 * 1000) {
+    // Missed reports for a long time. Maybe a connection or other communication error.
+    traffic_state = ERROR;
+    // No need to restart timer. We don't care about it in this state.
   }
 
   // Update RGB led status if needed. Note that no-change settings are
   // efficently ignored by the RGB module.rgb_module.
-  if (traffic_timeout) {
-    rgb_led::set(NO_TRAFFIC_COLOR, 1000);
+  if (traffic_state == BUSY) {
+    rgb_led::set(ACTIVE_COLOR, 0);  // same as Active below.
+  } else if (traffic_state != OK) {
+    rgb_led::set(NO_TRAFFIC_COLOR, 1000);  // error
   } else if (events & monitor::HAD_ERRORS) {
     rgb_led::set(ERRORS_COLOR, 300);
   } else if (events & monitor::REPORTED_ACTIVE) {
