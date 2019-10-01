a68279e13d83de6b8be1f431d4b6034dd2930dd1
Zapta <zapta@zapta.com>
2019-09-30 20:41:27 -0700

    Migrated to T4.

--- stepper_analyzer/arduino/arduino.ino
+++ stepper_analyzer/arduino/arduino.ino
@@ -2,9 +2,10 @@
 // Tested on Teensy 3.2, 96MHZ (overclocked)
 
 #include <stdio.h>
-#include <ADC.h>
+//#include <ADC.h>
 #include <Bounce.h>
-#include <FrequencyTimer2.h>
+//#include <FrequencyTimer2.h>
+#include<TimerOne.h>
 
 
 #include <ST7735_t3.h> // Hardware-specific library
@@ -28,10 +29,12 @@ const float COUNTS_PER_AMP = 0.2 * 4096 / 3.3;
 const int NON_ENERGIZED1 = 100;
 const int NON_ENERGIZED2 = 120;
 
-static ST7735_t3 tft(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+static const int adc1_pin = A9;  // = 23
+static const int adc2_pin = A3;  // = 17
 
-static const int adcPin1 = A9;
-static const int adcPin2 = A3;
+static const uint8_t adc1_pin_channel = 14;  
+static const uint8_t adc2_pin_channel = 11;  
+
 
 static const int LED1 = 1;
 static const int LED2 = 2;
@@ -43,87 +46,9 @@ static const int PUSH_BUTTON2 = 19;
 static Bounce push_button1 = Bounce(PUSH_BUTTON1, 10);  // 10 ms debounce
 static Bounce push_button2 = Bounce(PUSH_BUTTON2, 10);  // 10 ms debounce
 
-// Make it non heap. @@@@@ TODO
-//ADC *adc = new ADC();
-static ADC adc;
+static ST7735_t3 tft(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
-//elapsedMicros time;
-
-
-void adcTimingIsr() {
-    digitalWriteFast(LED3, true);
-
-  // Use static so we don't need to construct each time.
-  static ADC::Sync_result result;
-
-  result = adc.readSynchronizedSingle();
-
-  //timer_isr_count++;
-  adc.startSynchronizedSingleRead(adcPin1, adcPin2);
-
-  isr_process_adc_result(result);
-  
-  digitalWriteFast(LED3, false);
-}
-
-void setup() {
-  
-  Serial.begin(9600);
-
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-
-  digitalWriteFast(LED1, 0);
-  digitalWriteFast(LED2, 0);
-  digitalWriteFast(LED3, 0);
-
-  //adc_timing_timer(adc_timing_isr, 40);
-
-
-
-  pinMode(PUSH_BUTTON1, INPUT_PULLUP);
-  pinMode(PUSH_BUTTON2, INPUT_PULLUP);
-
-  // --- Display
-  tft.initR(INITR_BLACKTAB);
-  tft.fillScreen(ST7735_BLACK);
-  tft.setTextWrap(false);
-  
-  // --- ADC
-
-  pinMode(adcPin1, INPUT);
-  pinMode(adcPin2, INPUT);
-
-  adc.setReference(ADC_REFERENCE::REF_3V3, ADC_0);
-  adc.setReference(ADC_REFERENCE::REF_3V3, ADC_1);
-
-  adc.setAveraging(4, ADC_0);
-  adc.setAveraging(4, ADC_1);
-
-  adc.setResolution(12, ADC_0);
-  adc.setResolution(12, ADC_1);
-
-  adc.setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_0);
-  adc.setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_1);
-
-  adc.setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_0);
-  adc.setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_1);
-
-  // NOTE: this return a error status if pins are not supported.
-  adc.startSynchronizedSingleRead(adcPin1, adcPin2);
-  // Make sure the first timer ISR will find an adc result. 4 millis
-  // is an overkill
-  delay(4);
-
-  // FREQUENCYTIMER2_PIN = 5 on Teensy 3.2. Do not use for anything else.
-  pinMode(FREQUENCYTIMER2_PIN, OUTPUT);
-  FrequencyTimer2::setPeriod(20);  // 20usec -> 50k samplings/sec.
-  FrequencyTimer2::setOnOverflow(adcTimingIsr);
-  FrequencyTimer2::enable();
-}
-
-const int MAX_CAPTURE_SIZE = 5000;
+const int MAX_CAPTURE_SIZE = 500;
 static int16_t capture[MAX_CAPTURE_SIZE][2];
 
 struct IsrStatus {
@@ -153,121 +78,10 @@ struct IsrStatus {
 static  IsrStatus isr_status;
 
 
-void loop() {
-
-  const unsigned long period_ms = 100;
-  static unsigned long last_millis = millis();
-  unsigned long ms_left = period_ms - (millis() - last_millis);
-  if (ms_left > period_ms) { ms_left = period_ms; }  // in case of unsigned underflow
-  delay(ms_left);
-  last_millis += period_ms;
-  
-  // Reset
-  if (push_button1.update() && push_button1.fallingEdge()) {
-    Serial.println("Reset");
-    __disable_irq();
-    {
-      isr_status.non_energized_count = 0;
-      isr_status.full_steps = 0;
-      isr_status.quadrature_errors = 0;
-      isr_status.capture_size = 0;
-    }
-    __enable_irq();
-  }
-
-  // Take a snapshot of isr values and reset isr_count
-  IsrStatus _isr_status;
-  __disable_irq();
-  {
-    _isr_status = isr_status;
-    isr_status.isr_count = 0;
-  }
-  __enable_irq();
-
-
-  static char buffer[200];
-  tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
-
-  const int x0 = 25;
-  const int dy = 20;
-  int y = 20;
-  
-  tft.setCursor(x0, y);
-  sprintf(buffer, "A      %6.2f",  _isr_status.adc_val1  / COUNTS_PER_AMP);
-  tft.print(buffer);
-  y += dy;
-
-  tft.setCursor(x0, y);
-  sprintf(buffer, "B      %6.2f",  _isr_status.adc_val2  / COUNTS_PER_AMP);
-  tft.print(buffer);
-  y += dy;
-
-
-  tft.setCursor(x0, y);
-  sprintf(buffer, "ERRORS %6lu",  _isr_status.quadrature_errors);
-  tft.print(buffer);
-  y += dy;
-
-  tft.setCursor(x0, y);
-  sprintf(buffer, "POWER     %s",  _isr_status.is_energized? " ON" : "OFF");
-  tft.print(buffer);
-  y += dy;
-  
-  tft.setCursor(x0, y);
-  sprintf(buffer, "IDLES  %6lu",  _isr_status.non_energized_count);
-  tft.print(buffer);
-  y += dy;
-
-  tft.setTextColor(ST7735_YELLOW, ST7735_BLACK);
-
-  y += dy;
-  tft.setCursor(x0, y);
-  sprintf(buffer, "STEPS  %6d",  _isr_status.full_steps);
-  tft.print(buffer);
-
-
-  if (1) {
-    sprintf(buffer, "[%lu][%d][e:%lu] [%5d, %5d] [e:%d %lu] s:%d  steps:%d ",
-            _isr_status.isr_count, 
-            _isr_status.capture_size,
-            _isr_status.quadrature_errors,
-            _isr_status.adc_val1, _isr_status.adc_val2, _isr_status.is_energized, _isr_status.non_energized_count,
-            _isr_status.quadrant,  _isr_status.full_steps);
-  } else {
-    sprintf(buffer, "%d ", _isr_status.full_steps);
-  }
-  Serial.println(buffer);
-
-  // Print capture
-//  if (1) { 
-//    static boolean last_capture_full = false;
-//    boolean new_capture_full = (_isr_status.capture_size == MAX_CAPTURE_SIZE);
-//  
-//    if (new_capture_full && !last_capture_full) {
-//      // Marker
-//      Serial.println("-1000 -1000");
-//      Serial.println("0 0");
-//      for (int i = 0; i < isr_status.capture_size; i++) {
-//        Serial.print(capture[i][0]);
-//        Serial.print(" ");
-//        Serial.println(capture[i][1]);
-//      }
-//    }
-//    last_capture_full = new_capture_full;
-//  }
-
-  adc.printError();  // Print adc errors, if any
-}
-
-//adc0_isr;
-
 // Called from isr
-void isr_process_adc_result(ADC::Sync_result& result) {
-
-  // The casting to uint1_t is required only with 16 bit conversion
-  // to make sure the MSB bit is not signed extended to the 32bit ints.
-  const int v1 = ((int)((uint16_t)result.result_adc0)) - VAL1_OFFSET;
-  const int v2 = ((int)((uint16_t)result.result_adc1)) - VAL2_OFFSET;
+void isr_process_adc_results(int v1, int v2) {
+  v1 -= VAL1_OFFSET;
+  v2 -= VAL2_OFFSET;
 
   isr_status.isr_count++;
   isr_status.adc_val1 = v1;
@@ -347,6 +161,188 @@ void isr_process_adc_result(ADC::Sync_result& result) {
       isr_status.quadrant = new_quadrant;
     }
   }
+}
+
+// This ISR routine is invoked at samppling rate. It reads the two ADC
+// channels, start the conversion for next cycle and calls isr_process_adc_results() 
+// to process the values read.
+void adcTimingIsr() {
+  // For debugging
+  digitalWriteFast(LED3, true);
+
+  // Read ADC1, ADC2 values from previous cycle.
+  int result1;
+  if (ADC1_HS & ADC_HS_COCO0) {
+    result1 = ADC1_R0;
+  } else {
+    result1 = 1000;
+  }
+
+  int result2;
+  if (ADC2_HS & ADC_HS_COCO0) {
+      result2 = ADC2_R0;
+  } else {
+    // Should never get here.
+    result2 = 0;
+  }
+
+  // Start ADC1, ADC2 for next cycle
+  ADC2_HC0 = adc2_pin_channel;
+  ADC1_HC0 = adc1_pin_channel;
+
+  // Process results from previous cycle.
+  isr_process_adc_results(result1, result2);
+   
+  // For debugging  
+  digitalWriteFast(LED3, false);
+}
+
+void setup() {
+  // Serial
+  Serial.begin(9600);
+
+  // Leds
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
+  digitalWriteFast(LED1, 0);
+  digitalWriteFast(LED2, 0);
+  digitalWriteFast(LED3, 0);
+
+  // Buttons
+  pinMode(PUSH_BUTTON1, INPUT_PULLUP);
+  pinMode(PUSH_BUTTON2, INPUT_PULLUP);
+
+  // --- Display
+  tft.initR(INITR_BLACKTAB);
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextWrap(false);
+  
+  // --- ADC
+  pinMode(adc1_pin, INPUT);
+  pinMode(adc2_pin, INPUT);
+  analogReadRes(12);       // reading 12 bits
+  analogReadAveraging(1);  // TODO: consider to increase to 4
+
+  //--- timer
+  //
+  // TDOO: why setting PWM for pin TIMER1_B_PIN, the real output, doesn't work?
+  pinMode(TIMER1_B_PIN, OUTPUT);        // Timer output - pin 7.
+  Timer1.initialize(20);                // 20 us = 50 kHz
+  Timer1.pwm(TIMER1_A_PIN, 1024/4);     // 25% (abitrary)
+  Timer1.attachInterrupt(adcTimingIsr); // ISR
+}
 
 
+
+
+void loop() {
+  //for(;;) {
+  //  digitalWriteFast(LED1, !digitalReadFast(LED1));
+  //}
+
+ // Serial.println(adc1_pin);
+  
+  const unsigned long period_ms = 100;
+  static unsigned long last_millis = millis();
+  unsigned long ms_left = period_ms - (millis() - last_millis);
+  if (ms_left > period_ms) { ms_left = period_ms; }  // in case of unsigned underflow
+  delay(ms_left);
+  last_millis += period_ms;
+  
+  // Reset
+  if (push_button1.update() && push_button1.fallingEdge()) {
+    Serial.println("Reset");
+    __disable_irq();
+    {
+      isr_status.non_energized_count = 0;
+      isr_status.full_steps = 0;
+      isr_status.quadrature_errors = 0;
+      isr_status.capture_size = 0;
+    }
+    __enable_irq();
+  }
+
+  // Take a snapshot of isr values and reset isr_count
+  IsrStatus _isr_status;
+  __disable_irq();
+  {
+    _isr_status = isr_status;
+    isr_status.isr_count = 0;
+  }
+  __enable_irq();
+
+
+  static char buffer[200];
+  tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+
+  const int x0 = 25;
+  const int dy = 20;
+  int y = 20;
+  
+  tft.setCursor(x0, y);
+  sprintf(buffer, "A      %6.2f",  _isr_status.adc_val1  / COUNTS_PER_AMP);
+  tft.print(buffer);
+  y += dy;
+
+  tft.setCursor(x0, y);
+  sprintf(buffer, "B      %6.2f",  _isr_status.adc_val2  / COUNTS_PER_AMP);
+  tft.print(buffer);
+  y += dy;
+
+
+  tft.setCursor(x0, y);
+  sprintf(buffer, "ERRORS %6lu",  _isr_status.quadrature_errors);
+  tft.print(buffer);
+  y += dy;
+
+  tft.setCursor(x0, y);
+  sprintf(buffer, "POWER     %s",  _isr_status.is_energized? " ON" : "OFF");
+  tft.print(buffer);
+  y += dy;
+  
+  tft.setCursor(x0, y);
+  sprintf(buffer, "IDLES  %6lu",  _isr_status.non_energized_count);
+  tft.print(buffer);
+  y += dy;
+
+  tft.setTextColor(ST7735_YELLOW, ST7735_BLACK);
+
+  y += dy;
+  tft.setCursor(x0, y);
+  sprintf(buffer, "STEPS  %6d",  _isr_status.full_steps);
+  tft.print(buffer);
+
+
+//  if (1) {
+//    sprintf(buffer, "[%lu][%d][e:%lu] [%5d, %5d] [e:%d %lu] s:%d  steps:%d ",
+//            _isr_status.isr_count, 
+//            _isr_status.capture_size,
+//            _isr_status.quadrature_errors,
+//            _isr_status.adc_val1, _isr_status.adc_val2, _isr_status.is_energized, _isr_status.non_energized_count,
+//            _isr_status.quadrant,  _isr_status.full_steps);
+//  } else {
+//    sprintf(buffer, "%d ", _isr_status.full_steps);
+//  }
+//  Serial.println(buffer);
+
+  // Print capture buffer
+  if (1) { 
+    static boolean last_capture_full = false;
+    boolean new_capture_full = (_isr_status.capture_size == MAX_CAPTURE_SIZE);
+  
+    if (new_capture_full && !last_capture_full) {
+      // Marker
+      //Serial.println("-1000 -1000");
+      //Serial.println("0 0");
+      for (int i = 0; i < isr_status.capture_size; i++) {
+        Serial.print(capture[i][0] / COUNTS_PER_AMP);
+        Serial.print(" ");
+        Serial.println(capture[i][1] / COUNTS_PER_AMP );
+        delay(1);
+      }
+    }
+    last_capture_full = new_capture_full;
+  }
 }
