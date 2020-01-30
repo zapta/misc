93812005b1a3e714d64e9f65a8edd5b51e53578b
Zapta <zapta@users.noreply.github.com>
2020-01-29 21:26:29 -0800

    First version of the duet parser.

---
+++ duet3d_parser/duet3d_parser/duet_parser.h
@@ -0,0 +1,74 @@
+// Parse for the duet status json message. These are the 
+// messages returned from GET requests to 
+// http://xx.xx.xx.xx/rr_status?type=3
+
+
+#ifndef DUET_PARSER_H
+#define DUET_PARSER_H
+
+#include "json_parser.h"
+
+// Represents parsed duet status.
+struct DuetStatus {
+  char state_char;
+  int progress_permils;
+
+  void reset() {
+    // Default values.
+    state_char = ' ';
+    progress_permils = 0;
+  }
+};
+
+// Internal implementation of the duet parser. This class
+// is used to handle events from the json parser.
+class JsonParserListenerImpl : public JsonParserListener {
+ public:
+  void Reset();
+
+  virtual void OnReceivedValue(const char id[], const char val[],
+                               const int arrayDepth, const int indices[]);
+
+  virtual void OnArrayEnd(const char id[], const int arrayDepth,
+                          const int indices[]);
+
+  virtual void OnStartReceivedMessage();
+  virtual void OnEndReceivedMessage();
+  virtual void OnError();
+
+ private:
+  enum DuetParserState { IDLE = 1, IN_MESSAGE, MESSAGE_DONE, ERROR };
+  bool CheckExpectedState(DuetParserState expected_state);
+
+  DuetParserState duet_parser_state_;
+  DuetStatus captured_duet_status_;
+
+  friend class DuetParser;
+};
+
+// Public API of the duet status reponse parser. Same 
+// instance can be used for an unlimited of message
+// parsings.
+class DuetParser {
+ public:
+  // Start parsing a new json message.
+  void start_parsing_json_message();
+  // Prase the next char of the json message.
+  void ParseNextChar(const char c);
+
+  // Call this to check if the message was parsed ok.
+  bool is_message_ok() {
+    return json_listener_impl_.duet_parser_state_ ==
+           JsonParserListenerImpl::MESSAGE_DONE;
+  }
+  // If the message parsed ok, call this to get the parsed data.
+  const DuetStatus get_duet_state() {
+    return json_listener_impl_.captured_duet_status_;
+  }
+
+ private:
+  JsonParserListenerImpl json_listener_impl_;
+  JsonParser json_parser_;
+};
+
+#endif
---
+++ duet3d_parser/duet3d_parser/json_parser.cpp
@@ -0,0 +1,366 @@
+// Json parser. A slimmed down version of
+// https://github.com/dc42/PanelDueFirmware/blob/master/src/Hardware/SerialIo.cpp
+
+#include "json_parser.h"
+#include "simple_string.h"
+
+JsonParser::JsonState JsonParser::JsError() {
+  json_listener_->OnError();
+  return jsError;
+}
+
+void JsonParser::StartParsing(JsonParserListener* listener) {
+  state_ = jsBegin;
+  field_id_.clear();
+  field_val_.clear();
+  array_depth_ = 0;
+  json_listener_ = listener;
+}
+
+void JsonParser::RemoveLastId() {
+  int index = field_id_.size();
+  while (index != 0 && field_id_[index - 1] != '^' && field_id_[index - 1] != ':') {
+    --index;
+  }
+  field_id_.truncate(index);
+}
+
+void JsonParser::RemoveLastIdChar() {
+  if (field_id_.size() != 0) {
+    field_id_.truncate(field_id_.size() - 1);
+  }
+}
+
+bool JsonParser::InArray() {
+  return field_id_.size() > 0 && field_id_[field_id_.size() - 1] == '^';
+}
+
+void JsonParser::ProcessField() {
+  json_listener_->OnReceivedValue(field_id_.c_str(), field_val_.c_str(), array_depth_,
+                            array_indices_);
+  field_val_.clear();
+}
+
+void JsonParser::EndArray() {
+  json_listener_->OnArrayEnd(field_id_.c_str(), array_depth_, array_indices_);
+  if (array_depth_ != 0)  // should always be true
+  {
+    --array_depth_;
+    RemoveLastIdChar();
+  }
+}
+
+// Look for combining characters in the string value and convert them if
+// possible
+void JsonParser::ConvertUnicode() {
+  // Not needed for our limited application.
+}
+
+void JsonParser::ParseNextChar( char c) {
+  // For generality we treat line breaks as white space even
+  //  though the duet response is a single line.
+  if (c == '\n') {
+    c = ' ';
+  }
+
+  switch (state_) {
+    case jsBegin:  // initial state, expecting '{'
+      if (c == '{') {
+        json_listener_->OnStartReceivedMessage();
+        state_ = jsExpectId;
+        field_val_.clear();
+        field_id_.clear();
+        array_depth_ = 0;
+      }
+      break;
+
+    case jsExpectId:  // expecting a quoted ID
+      switch (c) {
+        case ' ':  // ignore space
+          break;
+        case '"':
+          state_ = jsId;
+          break;
+        case '}':  // empty object, or extra comma at end of field list
+          RemoveLastId();
+          if (field_id_.size() == 0) {
+            json_listener_->OnEndReceivedMessage();
+            state_ = jsBegin;
+          } else {
+            RemoveLastIdChar();
+            state_ = jsEndVal;
+          }
+          break;
+        default:
+          state_ = JsError();
+          break;
+      }
+      break;
+
+    case jsId:  // expecting an identifier, or in the middle of one
+      switch (c) {
+        case '"':
+          state_ = jsHadId;
+          break;
+        default:
+          if (c < ' ') {
+            state_ = JsError();
+          } else if (c != ':' && c != '^') {
+            if (!field_id_.add(c)) {
+              state_ = JsError();
+            }
+          }
+          break;
+      }
+      break;
+
+    case jsHadId:  // had a quoted identifier, expecting ':'
+      switch (c) {
+        case ':':
+          state_ = jsVal;
+          break;
+        case ' ':
+          break;
+        default:
+          state_ = JsError();
+          break;
+      }
+      break;
+
+    case jsVal:  // had ':' or ':[', expecting value
+      switch (c) {
+        case ' ':
+          break;
+        case '"':
+          field_val_.clear();
+          state_ = jsStringVal;
+          break;
+        case '[':
+          if (array_depth_ < MAX_ARRAY_NESTING && field_id_.add('^')) {
+            array_indices_[array_depth_] = 0;  // start an array
+            ++array_depth_;
+          } else {
+            state_ = JsError();
+          }
+          break;
+        case ']':
+          if (InArray()) {
+            EndArray();  // empty array
+            state_ = jsEndVal;
+          } else {
+            state_ = JsError();  // ']' received without a matching '[' first
+          }
+          break;
+        case '-':
+          field_val_.clear();
+          state_ = (field_val_.add(c)) ? jsNegIntVal : JsError();
+          break;
+        case '{':  // start of a nested object
+          state_ = (field_id_.add(':')) ? jsExpectId : JsError();
+          break;
+        default:
+          if (c >= '0' && c <= '9') {
+            field_val_.clear();
+            field_val_.add(c);  // must succeed because we just cleared fieldVal
+            state_ = jsIntVal;
+            break;
+          } else {
+            state_ = JsError();
+          }
+      }
+      break;
+
+    case jsStringVal:  // just had '"' and expecting a string value
+      switch (c) {
+        case '"':
+          ConvertUnicode();
+          ProcessField();
+          state_ = jsEndVal;
+          break;
+        case '\\':
+          state_ = jsStringEscape;
+          break;
+        default:
+          if (c < ' ') {
+            state_ = JsError();
+          } else {
+            field_val_.add(c);  // ignore any error so that long string parameters
+                              // just get truncated
+          }
+          break;
+      }
+      break;
+
+    case jsStringEscape:  // just had backslash in a string
+      if (!field_val_.full()) {
+        switch (c) {
+          case '"':
+          case '\\':
+          case '/':
+            if (!field_val_.add(c)) {
+              state_ = JsError();
+            }
+            break;
+          case 'n':
+          case 't':
+            if (!field_val_.add(' '))  // replace newline and tab by space
+            {
+              state_ = JsError();
+            }
+            break;
+          case 'b':
+          case 'f':
+          case 'r':
+          default:
+            break;
+        }
+      }
+      state_ = jsStringVal;
+      break;
+
+    case jsNegIntVal:  // had '-' so expecting a integer value
+      state_ = (c >= '0' && c <= '9' && field_val_.add(c)) ? jsIntVal : JsError();
+      break;
+
+    case jsIntVal:  // receiving an integer value
+      switch (c) {
+        case '.':
+          state_ = (field_val_.add(c)) ? jsFracVal : JsError();
+          break;
+        case ',':
+          ProcessField();
+          if (InArray()) {
+            ++array_indices_[array_depth_ - 1];
+            field_val_.clear();
+            state_ = jsVal;
+          } else {
+            RemoveLastId();
+            state_ = jsExpectId;
+          }
+          break;
+        case ']':
+          if (InArray()) {
+            ProcessField();
+            ++array_indices_[array_depth_ - 1];
+            EndArray();
+            state_ = jsEndVal;
+          } else {
+            state_ = JsError();
+          }
+          break;
+        case '}':
+          if (InArray()) {
+            state_ = JsError();
+          } else {
+            ProcessField();
+            RemoveLastId();
+            if (field_id_.size() == 0) {
+              json_listener_->OnEndReceivedMessage();
+              state_ = jsBegin;
+            } else {
+              RemoveLastIdChar();
+              state_ = jsEndVal;
+            }
+          }
+          break;
+        default:
+          if (!(c >= '0' && c <= '9' && field_val_.add(c))) {
+            state_ = JsError();
+          }
+          break;
+      }
+      break;
+
+    case jsFracVal:  // receiving a fractional value
+      switch (c) {
+        case ',':
+          ProcessField();
+          if (InArray()) {
+            ++array_indices_[array_depth_ - 1];
+            state_ = jsVal;
+          } else {
+            RemoveLastId();
+            state_ = jsExpectId;
+          }
+          break;
+        case ']':
+          if (InArray()) {
+            ProcessField();
+            ++array_indices_[array_depth_ - 1];
+            EndArray();
+            state_ = jsEndVal;
+          } else {
+            state_ = JsError();
+          }
+          break;
+        case '}':
+          if (InArray()) {
+            state_ = JsError();
+          } else {
+            ProcessField();
+            RemoveLastId();
+            if (field_id_.size() == 0) {
+              json_listener_->OnEndReceivedMessage();
+              state_ = jsBegin;
+            } else {
+              RemoveLastIdChar();
+              state_ = jsEndVal;
+            }
+          }
+          break;
+        default:
+          if (!(c >= '0' && c <= '9' && field_val_.add(c))) {
+            state_ = JsError();
+          }
+          break;
+      }
+      break;
+
+    case jsEndVal:  // had the end of a string or array value, expecting comma
+                    // or ] or }
+      switch (c) {
+        case ',':
+          if (InArray()) {
+            ++array_indices_[array_depth_ - 1];
+            field_val_.clear();
+            state_ = jsVal;
+          } else {
+            RemoveLastId();
+            state_ = jsExpectId;
+          }
+          break;
+        case ']':
+          if (InArray()) {
+            ++array_indices_[array_depth_ - 1];
+            EndArray();
+          } else {
+            state_ = JsError();
+          }
+          break;
+        case '}':
+          if (InArray()) {
+            state_ = JsError();
+          } else {
+            RemoveLastId();
+            if (field_id_.size() == 0) {
+              json_listener_->OnEndReceivedMessage();
+              state_ = jsBegin;
+            } else {
+              RemoveLastIdChar();
+              // state = jsEndVal;     // not needed, state == jsEndVal already
+            }
+          }
+          break;
+        default:
+          break;
+      }
+      break;
+
+    case jsError:
+      // Ignore all characters. State will be reset to jsBegin at the start of
+      // this function when we receive a newline.
+      break;
+  }
+}
+
+//}  // namespace parser
