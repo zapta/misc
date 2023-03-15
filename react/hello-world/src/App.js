f33bd00105e6cc6cd891c1a829e774985780562b
Zapta <zapta@users.noreply.github.com>
2023-03-14 21:31:15 -0700

    Basic scrolling chart working.

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -3,35 +3,34 @@ import './App.css';
 import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
 import Message from './components/Message';
+import MyChart from "./MyChart.js";
 
 import CanvasJSReact from './canvasjs.react';
 //var CanvasJSReact = require('./canvasjs.react');
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-const options = {
-  title: {
-    text: "Basic Column Chart in React"
-  },
-  data: [{
-    type: "column",
-    dataPoints: [
-      { label: "Apple", y: 10 },
-      { label: "Orange", y: 15 },
-      { label: "Banana", y: 25 },
-      { label: "Mango", y: 30 },
-      { label: "Grape", y: 28 }
-    ]
-  }]
-};
+
+// const options = {
+//   title: {
+//     text: "Basic Column Chart in React"
+//   },
+//   data: [{
+//     type: "column",
+//     dataPoints: [
+//       { label: "Apple", y: 10 },
+//       { label: "Orange", y: 15 },
+//       { label: "Banana", y: 25 },
+//       { label: "Mango", y: 30 },
+//       { label: "Grape", y: 28 }
+//     ]
+//   }]
+// };
 
 function App() {
   return (
     <div className="App">
-      {/* <Message name="bruce" /> */}
-      <CanvasJSChart options={options}
-      /* onRef = {ref => this.chart = ref} */
-      />
+      <MyChart />
     </div>
   );
 }
