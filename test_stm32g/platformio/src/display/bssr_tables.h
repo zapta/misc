113f80043ff983acfe09acdea82edbd849658218
Zapta <zapta@users.noreply.github.com>
2023-04-22 11:59:08 -0700

    initial check in of a test project.

---
+++ test_stm32g/platformio/src/display/bssr_tables.h
@@ -0,0 +1,11 @@
+// Lookup tables to drive the 16 bits parallel output.
+#pragma once
+
+#include <Arduino.h>
+
+namespace bssr_tables {
+
+extern const uint64_t direct_bssr_table[];
+extern const uint64_t color_bssr_table[];
+
+}  // namespace bssr_tables
