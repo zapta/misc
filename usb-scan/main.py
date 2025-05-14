cebac9c9659b2589598b3a3f6412619e3e392fa2
Zapta <zapta@users.noreply.github.com>
2025-05-14 10:54:49 -0700

    Changed from slices to starts.

--- usb-scan/main.py
+++ usb-scan/main.py
@@ -3,6 +3,14 @@ import re
 from typing import List, Tuple
 from dataclasses import dataclass
 
+TEXT="""
+empty
+Bus device vid:pid       probe type      manufacturer serial               product
+000 001    0x0403:0x6010 FTDI2232        AlhambraBits none                 Alhambra II v1.0A - B09-335
+001 001    0x0403:0x6010 FTDI2232        tinyVision.ai FT94RQ8V             UPduino v3.1c
+001 001    0x0403:0x6010 FTDI2232        tinyVision.ai.v3 FT94RQ8V             UPduino v3.1c
+"""
+
 HEADER_REGEX = re.compile(
     r"^(Bus *)(device *)(vid:pid *)(probe type *)(manufacturer *)(serial *)(product *)$"
 )
@@ -20,21 +28,36 @@ class DeviceInfo:
     serial_code: str
     description: str
 
+    def dump(self):
+        print(f"Device [{self.index}]")
+        print(f"    bus:     [{self.bus}]")
+        print(f"    device:  [{self.device}]")
+        print(f"    vid:     [{self.vid}]")
+        print(f"    pid:     [{self.pid}]")
+        print(f"    type:    [{self.type}]")
+        print(f"    manufct: [{self.manufacturer}]")
+        print(f"    serial:  [{self.serial_code}]")
+        print(f"    Desc:    [{self.description}]")
 
-def get_header_slices(header: str) -> List[slice]:
+
+def get_header_fields_starts(header: str) -> List[int]:
+    """Given an header line, returns the start indices of the columns
+    names. """
+
+    # The expected number of fields.
     N = 7
+    assert HEADER_REGEX.groups == N
 
     m = HEADER_REGEX.match(header)
     assert m
     assert m.lastindex == N
 
-    slices = []
+    fields_starts = []
     for i in range(N):
-        start = m.start(i + 1)
-        end = None if i == N - 1 else m.end(i + 1)
-        slices.append(slice(start, end))
+        fields_starts.append(m.start(i + 1))
 
-    return slices
+
+    return fields_starts
 
 
 def get_lines(text: str) -> Tuple[str, List[str]]:
@@ -52,29 +75,33 @@ def get_lines(text: str) -> Tuple[str, List[str]]:
     return (headers_lines[0], devices_lines)
 
 
-def extract_field(l: str, s):
-    value = l[s].strip()
+def extract_field(line:str, field_index: int, fields_starts:List[int]):
+    start = fields_starts[field_index]
+    end = fields_starts[field_index + 1] if field_index < (len(fields_starts) - 1) else None
+    value = line[slice(start, end)].strip()
     if value == "none":
         value = ""
     return value
 
 
 def get_devices(text: str) -> DeviceInfo:
-    header, devices_lines = get_lines(result.stdout)
-    slices = get_header_slices(header)
+    header, devices_lines = get_lines(text)
+    header_starts = get_header_fields_starts(header)
 
     devices = []
     for index, line in enumerate(devices_lines):
-        assert len(slices) == 7
+        assert len(header_starts) == 7
+
+        line_starts = header_starts.copy()
 
         # -- Extract fields
-        bus = extract_field(line, slices[0])
-        device = extract_field(line, slices[1])
-        vid_pid = extract_field(line, slices[2])
-        type = extract_field(line, slices[3])
-        manufacturer = extract_field(line, slices[4])
-        serial_code = extract_field(line, slices[5])
-        description = extract_field(line, slices[6])
+        bus = extract_field(line, 0, line_starts)
+        device = extract_field(line,  1, line_starts)
+        vid_pid = extract_field(line, 2, line_starts)
+        type = extract_field(line, 3, line_starts)
+        manufacturer = extract_field(line, 4, line_starts)
+        serial_code = extract_field(line, 5, line_starts)
+        description = extract_field(line, 6, line_starts)
 
         # Split pid_vid to pid and vid
         tokens = vid_pid.split(":")
@@ -100,89 +127,21 @@ def get_devices(text: str) -> DeviceInfo:
     return devices
 
 
-result = subprocess.run(
-    ["apio", "raw", "--", "openFPGALoader", "--scan-usb"],
-    capture_output=True,
-    text=True,
-)
+# result = subprocess.run(
+#     ["apio", "raw", "--", "openFPGALoader", "--scan-usb"],
+#     capture_output=True,
+#     text=True,
+# )
+# assert result.returncode == 0
+# text = result.stdout
 
-assert result.returncode == 0
+text = TEXT
 
-devices = get_devices(result.stdout)
+devices = get_devices(text)
 
 for device in devices:
-    print(f"DEVICE: {device}")
-
-#
-# header, devices_lines = get_lines(result.stdout)
-
-# print(f"{result.returncode=}")
-# print(f"{result.stdout}")
-
-# lines = result.stdout.splitlines()
+    print()
+    device.dump()
+print()
 
 
-# lines = [line for line in lines if line.strip() and line.strip() != "empty"]
-
-# header_lines = [line for line in lines if line.startswith("Bus")]
-# device_lines = [line for line in lines if not line.startswith("Bus")]
-
-# print(f"HEADER: [{header}]")
-
-# print()
-
-# for line in devices_lines:
-#     print(f"DEVICE: [{line}]")
-
-# assert len(header_lines) == 1
-# header = header_lines[0]
-
-# slices = get_header_slices(header)
-
-# print(f"{slices=}")
-
-# m = HEADER_REGEX.match(header)
-
-# for i, slice in enumerate(slices):
-#     text = header[slice]
-#     print()
-#     print(f"TEST  [{text}]")
-#     print(f"MATCH [{m.group(i+1)}]")
-
-
-# devices_rows = []
-# for line in devices_lines:
-#     fields = []
-#     for slice in slices:
-#         fields.append(line[slice].strip())
-#     devices_rows.append(fields)
-
-# for row in devices_rows:
-#     print(f"ROW {row}")
-
-
-# devices = []
-# for line in devices_lines:
-#     assert len(slices) == 7
-
-#     # -- Extract fields
-#     bus = extract_field(line, slices[0])
-#     device = extract_field(line, slices[1])
-#     vid_pid = extract_field(line, slices[2])
-#     type = extract_field(line, slices[3])
-#     manufacturer = extract_field(line, slices[4])
-#     serial_code = extract_field(line, slices[5])
-#     description = extract_field(line, slices[6])
-
-
-#     # Split pid_vid to pid and vid
-#     tokens = vid_pid.split(":")
-#     assert len(tokens) == 2, tokens
-#     assert tokens[0].startswith("0x")
-#     assert tokens[1].startswith("0x")
-#     vid = tokens[0][2:]
-#     pid = tokens[1][2:]
-
-#     device = DeviceInfo(bus=int(bus), device=int(device), vid=vid, pid=pid, type=type,
-#                     manufacturer=manufacturer, serial_code=serial_code, description=description)
-#     devices.append(device)
