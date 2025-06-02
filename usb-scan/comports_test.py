a8d9adea10fae9c73d520dd7cc39155e038bfd24
Zapta <zapta@users.noreply.github.com>
2025-06-02 11:41:34 -0700

    Added comports test

---
+++ usb-scan/comports_test.py
@@ -0,0 +1,19 @@
+
+# Usage:
+#   pip install pyserial
+#   python comports-test.py
+
+from serial.tools.list_ports import comports
+
+ports = comports()
+
+for port in ports:
+  if port.device and port.vid and port.pid:
+    print(
+        f"ID=[{port.vid:04X}:{port.pid:04X}] " + 
+        f"Port=[{port.device}] " + 
+        f"Manuf=[{port.manufacturer}] " + 
+        f"Desc=[{port.description}] " +
+        f"S/N=[{port.serial_number}]")
+
+
